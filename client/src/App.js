bda558cc73de443115903231fad5dab5eb4b5a97
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-14 20:05:25 +0530

    Added UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {Typography, AppBar} from '@material-ui/core';
+import VideoPlayer from "./VideoPlayer";
+import Options from "./Options";
+import Notifications from "./Notifications";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-        <h1>Hello, Anand</h1>
+    <div className={classes.wrapper}>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography variant="h2" align="center">
+                Video Chat
+            </Typography>
+           
+        </AppBar>
+        <VideoPlayer></VideoPlayer>
+        <Options>
+          <Notifications></Notifications>
+        </Options>
+      
     </div>
   );
 }
